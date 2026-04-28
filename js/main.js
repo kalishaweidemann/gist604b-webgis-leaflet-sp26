5e7c212a88877549c3a06919c05d850a9892c376
Kalisha Weidemann <kalishaweidemann@gmail.com>
2026-04-28 15:02:30 -0600

    Load park point features to map

--- js/main.js
+++ js/main.js
@@ -1,7 +1,55 @@
-var map = L.map('map').setView([42.36, -71.06], 13);
+// Create the Leaflet map and center it on Boston
+var map = L.map('map').setView([42.36, -71.06], 12);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// Add an OpenStreetMap basemap
+var osmBasemap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+// Add the basemap to the map
+osmBasemap.addTo(map);
+
+// Add a scale bar to the lower-left corner
+L.control.scale({
+  imperial: true,
+  metric: true
 }).addTo(map);
 
+// Create layer groups to organize datasets
+var parksLayer = L.layerGroup().addTo(map);
+var bikeLayer = L.layerGroup().addTo(map);
+var neighborhoodsLayer = L.layerGroup().addTo(map);
+
+// Load park point features
+fetch('data/boston_park_features_points.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+
+    // Convert GeoJSON into Leaflet layer
+    L.geoJSON(data, {
+
+      // Style points as circle markers
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng, {
+          radius: 5,
+          color: '#264653',
+          fillColor: '#2A9D8F',
+          fillOpacity: 0.8,
+          weight: 1
+        });
+      },
+
+      // Add popup using attribute data
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup(
+          '<strong>Park Feature</strong><br>' +
+          (feature.properties.Park_Name || 'Unnamed')
+        );
+      }
+
+    }).addTo(parksLayer);
+
+  });
