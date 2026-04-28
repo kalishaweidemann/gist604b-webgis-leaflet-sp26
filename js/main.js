e71aad6c6ea7adf8dcbb3a56f33dfb90fe5c6e3d
Kalisha Weidemann <kalishaweidemann@gmail.com>
2026-04-28 15:53:00 -0600

    Load bike network and city boundaries to map

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 // Create the Leaflet map and center it on Boston
-var map = L.map('map').setView([42.36, -71.06], 12);
+var map = L.map('map').setView([42.32, -71.06], 12);
 
 // Add an OpenStreetMap basemap
-var osmBasemap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
-
-// Add the basemap to the map
-osmBasemap.addTo(map);
+var osmBasemap = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
+  subdomains: 'abcd',
+  maxZoom: 20
+}).addTo(map);
 
 // Add a scale bar to the lower-left corner
 L.control.scale({
@@ -17,39 +15,133 @@ L.control.scale({
 }).addTo(map);
 
 // Create layer groups to organize datasets
-var parksLayer = L.layerGroup().addTo(map);
-var bikeLayer = L.layerGroup().addTo(map);
 var neighborhoodsLayer = L.layerGroup().addTo(map);
+var bikeLayer = L.layerGroup().addTo(map);
+var parksLayer = L.layerGroup().addTo(map);
 
-// Load park point features
+
+  // Load neighborhood polygon features
+fetch('data/boston_neighborhoods_polygons.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+
+    L.geoJSON(data, {
+
+      style: function(feature) {
+        return {
+          color: '#C9A227',
+          weight: 1.5,
+          opacity: 0.7,
+          fillColor: '#E9C46A',
+          fillOpacity: 0.12
+        };
+      },
+
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup(
+          '<strong>Neighborhood</strong><br>' +
+          '<strong>Name:</strong> ' + (feature.properties.name || 'Unnamed') + '<br>' +
+          '<strong>Area:</strong> ' + (feature.properties.sqmiles || 'N/A') + ' sq mi'
+        );
+      }
+
+    }).addTo(neighborhoodsLayer);
+
+  });
+
+
+    // Load bike network line features
+fetch('data/boston_bike_network_lines.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+
+    L.geoJSON(data, {
+
+      style: function(feature) {
+        return {
+          color: '#264653',
+          weight: 2.5,
+          opacity: 0.9
+        };
+      },
+
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup(
+          '<strong>Bike segment</strong><br>' +
+          '<strong>Street:</strong> ' + (feature.properties.STREET_NAM || 'Unnamed') + '<br>' +
+          '<strong>Facility:</strong> ' + (feature.properties.ExisFacil || 'N/A') + '<br>' +
+          '<strong>Installed:</strong> ' + (feature.properties.InstallDat || 'N/A')
+        );
+      }
+
+    }).addTo(bikeLayer);
+
+    bikeLayer.bringToFront();
+
+  });
+
+
+  // Load park point features
 fetch('data/boston_park_features_points.json')
   .then(function(response) {
     return response.json();
   })
   .then(function(data) {
 
-    // Convert GeoJSON into Leaflet layer
     L.geoJSON(data, {
 
-      // Style points as circle markers
       pointToLayer: function(feature, latlng) {
         return L.circleMarker(latlng, {
           radius: 5,
-          color: '#264653',
+          color: '#005F56',
           fillColor: '#2A9D8F',
-          fillOpacity: 0.8,
+          fillOpacity: 0.9,
           weight: 1
         });
       },
 
-      // Add popup using attribute data
       onEachFeature: function(feature, layer) {
         layer.bindPopup(
-          '<strong>Park Feature</strong><br>' +
+          '<strong>Park name</strong><br>' +
           (feature.properties.Park_Name || 'Unnamed')
         );
       }
 
-    }).addTo(parksLayer);
+    }).addTo(parksLayer).bringToFront();
 
   });
+
+
+  // Create a simple legend control
+var legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function(map) {
+  var div = L.DomUtil.create('div', 'legend');
+
+  div.innerHTML =
+    '<div><span class="legend-box neighborhoods"></span> Neighborhood boundaries</div>' +
+    '<div><span class="legend-line bike"></span> Bike network</div>' +
+    '<div><span class="legend-circle parks"></span> Park features</div>';
+
+  return div;
+};
+
+// Add legend to the map
+legend.addTo(map);
+
+
+// Layer controls
+var overlayMaps = {
+  'Neighborhood boundaries': neighborhoodsLayer,
+  'Bike network': bikeLayer,
+  'Park features': parksLayer
+};
+
+L.control.layers(null, overlayMaps, {
+  collapsed: true,
+  position: 'topleft'
+}).addTo(map);
